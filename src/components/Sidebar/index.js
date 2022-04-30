d346c323162ba0e3e0c6c82d62f1e16099a00ce6
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-04-30 00:24:16 -0500

    Add images and fonts in assets folder & some styling

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom';
+import './index.scss';
+import LogoS from '../../assets/images/logo-s.png'
+import LogoSubtitle from '../../assets/images/logo_sub.png'
+
+const Sidebar = () => (
+    <Link className='logo' to='/'>
+        <img src={LogoS} alt='logo' />
+        <img src={LogoSubtitle} alt='logo' />
+    </Link>
+)
+
+export default Sidebar;
