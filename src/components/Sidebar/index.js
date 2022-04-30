3849c5b0cbdd50acd738cb9a19809d7ce71ac257
Nicolas Bustos <jnbustosr@unal.edu.co>
2022-04-30 12:33:08 -0500

    Sidebar navigation improved

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -4,12 +4,13 @@ import LogoS from '../../assets/images/logo-s.png'
 import LogoSubtitle from '../../assets/images/logo_sub.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faEnvelope, faHome, faUser } from '@fortawesome/free-solid-svg-icons';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 const Sidebar = () => (
     <div className='nav-bar'>
         <Link className='logo' to='/'>
             <img src={LogoS} alt='logo' />
-            <img className='sub-logo' src={LogoSubtitle} alt='logo' />
+            <img className='sub-logo' src={LogoSubtitle} alt='slobodan' />
         </Link>
         <nav>
             <NavLink exact="true" activeclassname="active" to="/">
@@ -22,6 +23,18 @@ const Sidebar = () => (
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
+        <ul>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://www.linkedin.com/in/jnicobr/'>
+                    <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+                </a>
+            </li>
+            <li>
+                <a target="_blank" rel='noreferrer' href='https://github.com/jnicobr'>
+                    <FontAwesomeIcon icon={faGithub} color="#4d4d4e" />
+                </a>
+            </li>
+        </ul>
     </div>
 )
 
